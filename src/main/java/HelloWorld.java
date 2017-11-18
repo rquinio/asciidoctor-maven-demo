1e381af36184c3f7d96a8e501b2a7cd2f1590959
romain.quinio <romain.quinio@gmail.com>
2017-11-18 23:14:08 +0100

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Basic Java example
+ */
+public class HelloWorld {
+	// tag::main[]
+	public static void main(String[] args) { // <1>
+		System.out.println("Hello, World");
+	}
+	// end::main[]
+}
